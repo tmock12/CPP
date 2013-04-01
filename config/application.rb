2044f35b953a699de94e44a18e9cd226cdc805cf
Taylor Mock <tmock12@gmail.com>
2013-04-01 19:33:42 -0400

    New rails app

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require File.expand_path('../boot', __FILE__)
+
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
+
+if defined?(Bundler)
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+end
+
+module PropertyPortal
+  class Application < Rails::Application
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.encoding = "utf-8"
+    config.filter_parameters += [:password]
+    config.active_support.escape_html_entities_in_json = true
+    config.active_record.whitelist_attributes = true
+    config.assets.enabled = true
+    config.assets.version = '1.0'
+  end
+end
