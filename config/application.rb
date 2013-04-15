5385e22fecb897a8896f0164b9255043e53112d5
Taylor Mock <tmock12@gmail.com>
2013-04-14 23:20:10 -0400

    Dont initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module PropertyPortal
     config.encoding = "utf-8"
     config.assets.precompile += [ /\w+\.(?!css).+/, /\w+\.js$/ ]
     config.filter_parameters += [:password]
+    config.assets.initialize_on_precompile = false
     config.active_support.escape_html_entities_in_json = true
     config.active_record.whitelist_attributes = true
     config.assets.enabled = true
