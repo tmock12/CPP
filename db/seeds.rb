072c2a4633b18833544f2eef688bf9408c82d824
Taylor Mock <tmock12@gmail.com>
2013-04-18 22:53:21 -0400

    Add contact seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+contacts = [
+  ['Furman Wood', '404 803 8412', 'fwood@cpprofessionals.com'],
+  ['Daniel Levison', '404 358 3547', 'dlevison@cpprofessionals.com'],
+  ['Keith Guthrie', '404 790 7271', 'kguthrie@CPProfessionals.com'],
+  ['William Wood', '404 867 9110', 'wwood@cpprofessionals.com'],
+  ['Jason Whitworth', '404 723 9199', 'jwhitworth@CPProfessionals.com'],
+  ['Steve Hanna', '404 421 0151', 'sthanna@cpprofessionals.com'],
+  ['Alex Cobble', '404 307 4837', 'acobble@cpprofessionals.com'],
+  ['Rachel Condon', '404 275 5794', 'rcondon@cpprofessionals.com'],
+  ['Stewart Wallace', '404 641 0424', 'swallace@cpprofessionals.com'],
+  ['Nick Carlson', '770 656 4612', 'ncarlson@cpprofessionals.com']
+]
+
+contacts.each do |contact|
+  Contact.create(name: contact[0], cell: contact[1], email: contact[2])
+end
