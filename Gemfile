c929917b4777e8b933153366ad4c410e02689020
Taylor Mock <tmock12@gmail.com>
2013-06-02 09:05:48 -0400

    Use capybara webkit

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ end
 group :test do
   gem 'cucumber-rails', :require => false
   gem 'database_cleaner'
+  gem 'capybara-webkit'
 end
 
 group :development, :test do
--- features/support/env.rb
+++ features/support/env.rb
@@ -51,6 +51,8 @@ end
 #   end
 #
 
+Capybara.javascript_driver = :webkit
+
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
