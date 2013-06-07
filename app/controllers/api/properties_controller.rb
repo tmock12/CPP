f38033c3aa0118925c454e20cca2a423498b3da2
Taylor Mock <tmock12@gmail.com>
2013-06-06 21:52:49 -0400

    API controller inherits from ActionController::Base

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -1,7 +1,13 @@
-class Api::PropertiesController < ApplicationController
+class Api::PropertiesController < ActionController::Base
+  before_filter :set_access_control_headers
   expose(:property) { Property.sampler }
 
   def index
     render json: property, each_serializer: ApiPropertiesSerializer
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://www.cpprofessionals.com'
+    headers['Access-Control-Request-Method'] = '*'
   end
 end
