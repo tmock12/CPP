65688dedbfa9d77cf16e416d29e537c26df0a6be
Taylor Mock <tmock12@gmail.com>
2013-06-11 21:58:39 -0400

    Order properties in admin dashboard by numbers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
   before_filter :require_user
 
-  expose(:properties) { Property.order("LOWER(title)") }
+  expose(:properties) { properties }
   expose(:property)
+
+  private
+
+  def properties
+    sorted_properties_with_numbers + properties_without_numbers
+  end
+
+  def all_properties
+    Property.order("LOWER(title)")
+  end
+
+  def properties_with_numbers
+    all_properties.select { |p| p.title[/\A\d+/] }
+  end
+
+  def properties_without_numbers
+    all_properties - properties_with_numbers
+  end
+
+  def sorted_properties_with_numbers
+    properties_with_numbers.sort { |a, b| a.title.to_i <=> b.title.to_i }
+  end
 end
