7ff20a4ba9aab043ac8776d619e9e2c5f6f01052
Taylor Mock <tmock12@gmail.com>
2013-07-05 14:26:05 -0400

    Server compiles and static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ PropertyPortal::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.compress = true
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
